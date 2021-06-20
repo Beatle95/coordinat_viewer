89eb4d89fca9720eb98446919fff24cdc6469651
shark <tishenko.vlad.apc@gmail.com>
2021-06-20 21:43:05 +0300

    Added scene graph and textures for Earth model.

--- include/Application.h
+++ include/Application.h
@@ -1,7 +1,10 @@
+#include <Magnum/ImageView.h>
 #include <Magnum/GL/Buffer.h>
 #include <Magnum/GL/DefaultFramebuffer.h>
 #include <Magnum/GL/Mesh.h>
 #include <Magnum/GL/Renderer.h>
+#include <Magnum/GL/Texture.h>
+#include <Magnum/GL/TextureFormat.h>
 #include <Magnum/Math/Color.h>
 #include <Magnum/Math/Matrix4.h>
 #include <Magnum/MeshTools/Interleave.h>
@@ -9,28 +12,74 @@
 #include <Magnum/Platform/Sdl2Application.h>
 #include <Magnum/Primitives/Cube.h>
 #include <Magnum/Shaders/PhongGL.h>
-#include <Magnum/Trade/MeshData.h>
 #include <Magnum/MeshTools/Compile.h>
 #include <Magnum/Trade/AbstractImporter.h>
+#include <Magnum/Trade/ImageData.h>
+#include <Magnum/Trade/MeshData.h>
+#include <Magnum/Trade/MeshObjectData3D.h>
+#include <Magnum/Trade/PhongMaterialData.h>
+#include <Magnum/Trade/SceneData.h>
+#include <Magnum/Trade/TextureData.h>
+#include <Magnum/SceneGraph/Camera.h>
+#include <Magnum/SceneGraph/Drawable.h>
+#include <Magnum/SceneGraph/MatrixTransformation3D.h>
+#include <Magnum/SceneGraph/Scene.h>
 
 #define LOADING_ERROR 1
     
 using namespace Magnum;
 using namespace Magnum::Math::Literals;
 
-class CoordinatViewer: public Platform::Application {
-    public:
-        explicit CoordinatViewer(const Arguments& arguments);
+typedef SceneGraph::Object<SceneGraph::MatrixTransformation3D> Object3D;
+typedef SceneGraph::Scene<SceneGraph::MatrixTransformation3D> Scene3D;
 
-    private:
-        void drawEvent() override;
-        void mousePressEvent(MouseEvent& event) override;
-        void mouseReleaseEvent(MouseEvent& event) override;
-        void mouseMoveEvent(MouseMoveEvent& event) override;
+class CoordinatViewer: public Platform::Application 
+{
+public:
+    explicit CoordinatViewer(const Arguments& arguments);
 
-        GL::Mesh mEarthMesh;
-        Shaders::PhongGL mEarthShader;
+private:
+    void drawEvent() override;
+    void mousePressEvent(MouseEvent& event) override;
+    void mouseReleaseEvent(MouseEvent& event) override;
+    void mouseMoveEvent(MouseMoveEvent& event) override;
+    void viewportEvent(ViewportEvent& event) override;
 
-        Matrix4 mTranssformation, mProjection;
-        Color3 mColor;
+    Vector3 positionOnSphere(const Vector2i& position);
+
+    GL::Mesh mEarthMesh;
+    GL::Texture2D mEarthTexture;
+    Shaders::PhongGL mTextureShader{Shaders::PhongGL::Flag::DiffuseTexture};
+
+    Scene3D mScene;
+    Object3D mManimpulator, mCameraObject;
+    SceneGraph::Camera3D* mCamera;
+    SceneGraph::DrawableGroup3D mDrawables;
+    Vector3 mPreviousPosition;
+    Object3D* mSceneLightObj;
+};
+
+class TexturedDrawable: public SceneGraph::Drawable3D 
+{
+public:
+    explicit TexturedDrawable(
+        Object3D& object, 
+        Shaders::PhongGL& shader, 
+        GL::Mesh& mesh, 
+        GL::Texture2D& texture, 
+        SceneGraph::DrawableGroup3D& group,
+        Object3D* sceneLightObj)
+        : SceneGraph::Drawable3D{object, &group}, 
+        mShader(shader), 
+        mMesh(mesh), 
+        mTexture(texture),
+        mSceneLightObj(sceneLightObj) {}
+
+private:
+    void draw(const Matrix4& transformationMatrix, SceneGraph::Camera3D& camera) override;
+
+    Shaders::PhongGL& mShader;
+    GL::Mesh& mMesh;
+    GL::Texture2D& mTexture;
+    Object3D* mSceneLightObj;
 };
--- src/Application.cpp
+++ src/Application.cpp
@@ -1,11 +1,33 @@
 #include "Application.h"
 
 CoordinatViewer::CoordinatViewer(const Arguments& arguments):
-    Platform::Application{arguments, Configuration{}
-        .setTitle("Magnum Primitives Example")}
+    Platform::Application{arguments, 
+        Configuration{}
+        .setTitle("Coordinat Viewer")
+        .setSize(Vector2i{800, 800})
+        .setWindowFlags(Magnum::Platform::Sdl2Application::Configuration::WindowFlag::Resizable)}
 {
+    /* Every scene needs a camera */
+    mCameraObject
+        .setParent(&mScene)
+        .translate(Vector3{0.0f, 0.0f, 20.0f});
+    mCamera = new SceneGraph::Camera3D{mCameraObject};
+    (*mCamera)
+        .setAspectRatioPolicy(SceneGraph::AspectRatioPolicy::Extend)
+        .setProjectionMatrix(Matrix4::perspectiveProjection(35.0_degf, 1.0f, 0.01f, 1000.0f))
+        .setViewport(GL::defaultFramebuffer.viewport().size());
+
+    /* Base object, parent of all (for easy manipulation) */
+    mManimpulator.setParent(&mScene);
+    mSceneLightObj = new Object3D(&mManimpulator);
+    mSceneLightObj->translate(Vector3{0.0f, 0.0f, 30.0f});
+
+    // set renderer and shader defaults
     GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
     GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
+    mTextureShader.setAmbientColor(0x111111_rgbf)
+        .setSpecularColor(0x777777_rgbf)
+        .setShininess(80.0f);
 
     PluginManager::Manager<Trade::AbstractImporter> manager;
     auto assimpImporter = manager.loadAndInstantiate("AssimpImporter");
@@ -13,62 +35,94 @@ CoordinatViewer::CoordinatViewer(const Arguments& arguments):
         Error{} << "Cannot load Earth.fbx";
         exit(LOADING_ERROR);
     }
+    auto pngImporter = manager.loadAndInstantiate("PngImporter");
+    if (!pngImporter->openFile("./resources/Diffuse.png")) {
+        Error{} << "Cannot load Diffuse.png";
+        exit(LOADING_ERROR);
+    }
 
     if (assimpImporter->meshCount() < 1) {
         Error{} << "Cannot find meshes in Earth.fbx";
         exit(LOADING_ERROR);
     }
+    auto meshData = assimpImporter->mesh(0);
+    mEarthMesh = MeshTools::compile(*meshData);
 
-    auto mesh_data = assimpImporter->mesh(0);
-    mEarthMesh = MeshTools::compile(*mesh_data);
+    if (pngImporter->image2DCount() < 1) {
+        Error{} << "Cannot find images in Diffuse.png";
+        exit(LOADING_ERROR);
+    }
+    auto image = pngImporter->image2D(0);
+    
+    mEarthTexture.setMagnificationFilter(GL::SamplerFilter::Linear)
+        .setMinificationFilter(GL::SamplerFilter::Linear, GL::SamplerMipmap::Linear)
+        .setWrapping(GL::SamplerWrapping::ClampToEdge)
+        .setMaxAnisotropy(GL::Sampler::maxMaxAnisotropy())
+        .setStorage(1, GL::textureFormat(image->format()), image->size())
+        .setSubImage(0, {}, *image);
 
-    mTranssformation =
-        Matrix4::rotationX(30.0_degf)*Matrix4::rotationY(40.0_degf);
-    mProjection =
-        Matrix4::perspectiveProjection(
-            35.0_degf, Vector2{windowSize()}.aspectRatio(), 0.01f, 100.0f)*
-        Matrix4::translation(Vector3::zAxis(-30.0f));
-    mColor = Color3::fromHsv({35.0_degf, 1.0f, 1.0f});
+    auto* object = new Object3D(&mManimpulator);
+    new TexturedDrawable(*object, mTextureShader, mEarthMesh, mEarthTexture, mDrawables, mSceneLightObj);
 }
 
-void CoordinatViewer::drawEvent() {
-    GL::defaultFramebuffer.clear(
-        GL::FramebufferClear::Color|GL::FramebufferClear::Depth);
-
-    mEarthShader.setLightPositions({{1.4f, 1.0f, 0.75f, 0.0f}})
-        .setDiffuseColor(mColor)
-        .setAmbientColor(Color3::fromHsv({mColor.hue(), 1.0f, 0.3f}))
-        .setTransformationMatrix(mTranssformation)
-        .setNormalMatrix(mTranssformation.normalMatrix())
-        .setProjectionMatrix(mProjection)
-        .draw(mEarthMesh);
-
+void CoordinatViewer::drawEvent() 
+{
+    GL::defaultFramebuffer.clear(GL::FramebufferClear::Color|GL::FramebufferClear::Depth);
+    mCamera->draw(mDrawables);
     swapBuffers();
 }
 
-void CoordinatViewer::mousePressEvent(MouseEvent& event) {
-    if(event.button() != MouseEvent::Button::Left) return;
-
-    event.setAccepted();
+void CoordinatViewer::mousePressEvent(MouseEvent& event) 
+{
+    if(event.button() != MouseEvent::Button::Left)
+        mPreviousPosition = positionOnSphere(event.position());
 }
 
-void CoordinatViewer::mouseReleaseEvent(MouseEvent& event) {
-    mColor = Color3::fromHsv({mColor.hue() + 50.0_degf, 1.0f, 1.0f});
-
-    event.setAccepted();
-    redraw();
+void CoordinatViewer::mouseReleaseEvent(MouseEvent& event) 
+{
+    if(event.button() != MouseEvent::Button::Left)
+        mPreviousPosition = Vector3();
 }
 
-void CoordinatViewer::mouseMoveEvent(MouseMoveEvent& event) {
+void CoordinatViewer::mouseMoveEvent(MouseMoveEvent& event) 
+{
     if(!(event.buttons() & MouseMoveEvent::Button::Left)) return;
 
-    Vector2 delta = 3.0f*Vector2{event.relativePosition()}/Vector2{windowSize()};
+    const Vector3 currentPosition = positionOnSphere(event.position());
+    const Vector3 axis = Math::cross(mPreviousPosition, currentPosition);
 
-    mTranssformation =
-        Matrix4::rotationX(Rad{delta.y()})*
-        mTranssformation*
-        Matrix4::rotationY(Rad{delta.x()});
+    //if(mPreviousPosition.length() < 0.001f || axis.length() < 0.001f) return;
 
-    event.setAccepted();
+    mManimpulator.rotate(Math::angle(mPreviousPosition, currentPosition), axis.normalized());
+    mPreviousPosition = currentPosition;
+
     redraw();
+}
+
+void CoordinatViewer::viewportEvent(ViewportEvent& event) 
+{    
+    GL::defaultFramebuffer.setViewport({{}, event.framebufferSize()});
+    mCamera->setViewport(event.windowSize());
+}
+
+Vector3 CoordinatViewer::positionOnSphere(const Vector2i& position) 
+{
+    const Vector2 positionNormalized = Vector2{position}/Vector2{mCamera->viewport()} - Vector2{0.5f};
+    const Float length = positionNormalized.length();
+    const Vector3 result(length > 1.0f ? Vector3(positionNormalized, 0.0f) : Vector3(positionNormalized, 1.0f - length));
+    return (result*Vector3::yScale(-1.0f)).normalized();
+}
+
+void TexturedDrawable::draw(const Matrix4& transformationMatrix, SceneGraph::Camera3D& camera) 
+{
+    auto lightPos = mSceneLightObj->absoluteTransformation() * Vector4{0.0f, 10.0f, 10.0f, 0.0f};
+    mShader
+    .setLightPositions({
+        lightPos
+    })
+    .setTransformationMatrix(transformationMatrix)
+    .setNormalMatrix(transformationMatrix.normalMatrix())
+    .setProjectionMatrix(camera.projectionMatrix())
+    .bindDiffuseTexture(mTexture)
+    .draw(mMesh);
 }
