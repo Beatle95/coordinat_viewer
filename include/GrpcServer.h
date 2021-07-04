c9ec9f2aae50bbfffbd2679fcb9d89005763dda3
shark <tishenko.vlad.apc@gmail.com>
2021-07-04 23:05:55 +0300

    Add GRPC server class.

---
+++ include/GrpcServer.h
@@ -0,0 +1,12 @@
+#ifndef GRPC_SERVER
+#define GRPC_SERVER
+#include <grpcpp/grpcpp.h>
+#include "CoordinateViewer.grpc.pb.h"
+#include "CoordinateViewer.pb.h"
+
+final class GrpcServer : public CoordinateViewerService {
+public:
+private:
+}
+
+#endif
